99526ab64ebe73ec4b844fccb48b748cc4d8c2c1
Sebastian Mondrup <semon20@student.sdu.dk>
2021-04-13 14:46:49 +0200

    Made fxml for viewing credits

--- src/main/java/presentation/Controller.java
+++ src/main/java/presentation/Controller.java
@@ -3,8 +3,19 @@ package presentation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    public ListView productionListview;
+    public Button previousButton;
+    public Button nextButton;
+    public TextArea displayArea;
+    public Button editProductionButton;
+    public Button addProductionButton;
+    public Button searchButton;
+    public TextField searchField;
     @FXML
     private Button winButton;
 
