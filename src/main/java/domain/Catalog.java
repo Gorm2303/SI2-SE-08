c735618a796fe87c0ce5fbfe0596e180b52138a4
Mikkel <mikke20@student.sdu.dk>
2021-04-20 10:16:32 +0200

    implementation from class diagram

--- src/main/java/domain/Catalog.java
+++ src/main/java/domain/Catalog.java
@@ -1,4 +1,34 @@
 package domain;
 
+import java.util.Date;
+
 public class Catalog {
+
+    public Production showProduction(int productionId){
+        return null;
+    }
+    public void addProduction(String name, int id, Organization producer, Date releaseDate, String programCategory, int length){
+
+    }
+    public void removeProduction(int productionId){
+
+    }
+    public Contributor showContributor(int contributorId){
+        return null;
+    }
+    public void addContributor(String name, int id, Date birthDate){
+
+    }
+    public void removeContributor(int contributorId){
+
+    }
+    public Organization showOrganization(int organizationId){
+        return null;
+    }
+    public void addOrganization(String name, int id){
+
+    }
+    public void removeOrganization(int organizationId){
+
+    }
 }
--- src/main/java/domain/Contributor.java
+++ src/main/java/domain/Contributor.java
@@ -1,4 +1,44 @@
 package domain;
 
+import java.util.Date;
+
 public class Contributor {
+    private String name;
+    private int id;
+    private Date birthDate;
+
+    public Contributor(String name, int id, Date birthDate) {
+        this.name = name;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
 }
