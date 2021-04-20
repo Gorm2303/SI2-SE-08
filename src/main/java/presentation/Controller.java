03c2047fb593d16006b04e1e8de843de5d6b313e
Gorm2303 <gokri20@student.sdu.dk>
2021-04-20 13:33:12 +0200

    NewProductionController has functioning buttons

--- src/main/java/presentation/Controller.java
+++ src/main/java/presentation/Controller.java
@@ -3,7 +3,10 @@ package presentation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
@@ -12,17 +15,14 @@ public class Controller {
     public TextArea displayArea;
     public TextField searchField;
     @FXML
-    private Button btnShowCredits, editProductionButton, addProductionButton, newOrganization, newContributor, newOrganizationCancel, newOrganizationSave,
-            searchButton, nextButton, previousButton, saveProduction, productionCancelChanges, deleteProduction;
-    @FXML
-    private Label birthdayLabel;
-    @FXML
-    private DatePicker birthdaySelect;
+    private Button btnShowCredits, editProductionButton, addProductionButton, searchButton, nextButton, previousButton;
+
 
     public void onButtonClicked(ActionEvent actionEvent) {
         Button button = (Button) actionEvent.getSource();
 
-        if (button == addProductionButton || button == newOrganizationCancel) {
+        // Buttons for browsing the program
+        if (button == addProductionButton) {
             try {
                 Scene scene = new Scene(Main.loadFXML("newproduction"), 810, 500);
                 Main.getPrimaryStage().setScene(scene);
@@ -30,15 +30,7 @@ public class Controller {
                 e.printStackTrace();
             }
 
-        } else if (button == newOrganization || button == newContributor) {
-            try {
-                Scene scene = new Scene(Main.loadFXML("makecharacter"), 600, 500);
-                Main.getPrimaryStage().setScene(scene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } else if (button == btnShowCredits || button == productionCancelChanges) {
+        } else if (button == btnShowCredits) {
             try {
                 Scene scene = new Scene(Main.loadFXML("showcredit"), 600, 500);
                 Main.getPrimaryStage().setScene(scene);
@@ -48,5 +40,4 @@ public class Controller {
             }
         }
     }
-
 }
