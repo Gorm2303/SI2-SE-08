4fc95375706cbfe1f20069aae6f354e8c0c784a4
Sebastian Mondrup <semon20@student.sdu.dk>
2021-04-20 13:40:23 +0200

    Added functionality to add production to datalayer, and implemented idmanager

--- src/main/java/data/DataFacade.java
+++ src/main/java/data/DataFacade.java
@@ -5,15 +5,16 @@ import Acquaintance.IOrganization;
 import Acquaintance.IProduction;
 import domain.Production;
 
-public class DataFacade {
+import java.util.ArrayList;
+
+public class DataFacade implements IDataFacade{
     private Data data;
-    private IDManager idManager;
 
     public DataFacade () {
         this.data = new Data();
-        this.idManager = new IDManager();
     }
 
+    @Override
     public IProduction getProduction(int id) {
         for (IProduction production : data.getProductions()) {
             if (production.getId() == id) {
@@ -23,6 +24,7 @@ public class DataFacade {
         return null;
     }
 
+    @Override
     public IOrganization getOrganization(int id) {
         for (IOrganization organization : data.getOrganizations()) {
             if (organization.getId() == id) {
@@ -32,6 +34,7 @@ public class DataFacade {
         return null;
     }
 
+    @Override
     public IContributor getContributor(int id) {
         for (IContributor contributor : data.getContributors()) {
             if (contributor.getId() == id) {
@@ -41,28 +44,49 @@ public class DataFacade {
         return null;
     }
 
+    @Override
     public void addProduction(IProduction production) {
         data.getProductions().add(production);
     }
 
+    @Override
     public void addOrganization(IOrganization organization) {
         data.getOrganizations().add(organization);
     }
 
+    @Override
     public void addContributor(IContributor contributor) {
         data.getContributors().add(contributor);
     }
 
+    @Override
     public void removeProduction(IProduction production) {
         data.getProductions().remove(production);
     }
 
+    @Override
     public void removeOrganization(IOrganization organization) {
         data.getOrganizations().remove(organization);
     }
 
+    @Override
     public void removeContributor(IContributor contributor) {
         data.getContributors().remove(contributor);
+    }
+
+    @Override
+    public int getProductionId() {
+        return IDManager.getProductionId();
+    }
+
+    @Override
+    public int getOrganizationId() {
+        return IDManager.getOrganizationId();
+    }
+
+    @Override
+    public int getContributorId() {
+        return IDManager.getContributorId();
     }
 
 
---
+++ src/main/java/data/IDataFacade.java
@@ -0,0 +1,22 @@
+package data;
+
+import Acquaintance.IContributor;
+import Acquaintance.IOrganization;
+import Acquaintance.IProduction;
+
+import java.util.ArrayList;
+
+public interface IDataFacade {
+    IProduction getProduction(int id);
+    IOrganization getOrganization(int id);
+    IContributor getContributor(int id);
+    void addProduction(IProduction production);
+    void addOrganization(IOrganization organization);
+    void addContributor(IContributor contributor);
+    void removeProduction(IProduction production);
+    void removeOrganization(IOrganization organization);
+    void removeContributor(IContributor contributor);
+    int getProductionId();
+    int getOrganizationId();
+    int getContributorId();
+}
